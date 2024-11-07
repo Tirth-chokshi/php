f6e12a3bceff0b5c62e3fcdf8615ae34d6c31e2f
Tirth chokshi <122982506+Tirth-chokshi@users.noreply.github.com>
2024-11-07 07:24:18 +0530

    Create interface.php

---
+++ interface.php
@@ -0,0 +1,34 @@
+Write a PHP interface called 'Resizable' with a method 'resize()'. Implement the 'Resizable' interface in a class called 'Square' and add functionality to resize the square.
+<?php
+interface Resizable {
+    public function resize($percentage);
+}
+
+class Square implements Resizable {
+    private $side;
+
+    public function __construct($side) {
+        $this->side = $side;
+    }
+
+    public function resize($percentage) {
+        $this->side = $this->side * ($percentage / 100);
+    }
+
+    public function getArea() {
+        return pow($this->side, 2);
+    }
+
+    public function getSide() {
+        return $this->side;
+    }
+}
+
+$square = new Square(10);
+echo "Initial Side Length: " . $square->getSide() . "</br>";
+
+$square->resize(60); // Resize the square to 60% of its original size
+echo "Resized Side Length: " . $square->getSide() . "</br>";
+
+echo "Area: " . $square->getArea() . "</br>";
+?>
